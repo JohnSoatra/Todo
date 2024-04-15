d7256fad4cba6460fd06300a10a40bf63a575c6b
soatra <johnsoatra@gmail.com>
2024-04-15 17:32:37 +0700

    constants and utils

---
+++ constants/vars.ts
@@ -0,0 +1,6 @@
+const Vars = {
+    MaxTodoCharacterLength: 50,
+    MaxAllTodoLength: 100
+}
+
+export default Vars;
---
+++ utils/api.ts
@@ -0,0 +1,5 @@
+function api(path: string) {
+    return '/api/' + path;
+}
+
+export default api;
